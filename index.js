fa40fb18340ee16b4e7c806e484356f720bf65e5
chamamaxnew <my2008g@gmail.com>
2025-12-24 17:05:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get("/pair", (req, res) => {
   res.sendFile(path.join(__path, "pair.html"));
 });
 
-app.get("/", (req, res) => {
+app.get("/pair", (req, res) => {
   res.sendFile(path.join(__path, "main.html"));
 });
 
