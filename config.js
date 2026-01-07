1a920705e7a76a831a8dfbebb89dda21d9968392
surangasutiya-stack <surangasutiya@gmail.com>
2026-01-07 07:39:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,20 +2,46 @@ module.exports = {
     AUTO_VIEW_STATUS: 'true',
     AUTO_LIKE_STATUS: 'true',
     AUTO_RECORDING: 'true',
-    AUTO_LIKE_EMOJI: ['🧩', '🍉', '💜', '🌸', '🪴', '💊', '💫', '🍂', '🌟', '🎋', '😶‍🌫️', '🫀', '🧿', '👀', '🤖', '🚩', '🥰', '🗿', '💜', '💙', '🌝', '🖤', '💚'],
+
+    // 🔥 AUTO REACT SETTINGS
+    AUTO_REACT: 'true',
+
+    OWNER_AUTO_REACT: 'true',
+    OWNER_REACT_EMOJI: ['👑','💜','🔥','🫶','✨'], // Owner reacts with random emoji
+    OWNER_NUMBER: '94771657914', // +94 77 165 7914
+
+    USER_AUTO_REACT: 'true',
+    USER_REACT_LIST: [
+        '9477XXXXXXX', // put other user numbers here
+        '9471XXXXXXX'
+    ],
+    USER_REACT_EMOJI: ['💜','🔥','🥰','✨','🫶'], // users react with random emoji
+
+    AUTO_LIKE_EMOJI: [
+        '🧩','🍉','💜','🌸','🪴','💊','💫','🍂','🌟','🎋',
+        '😶‍🌫️','🫀','🧿','👀','🤖','🚩','🥰','🗿','💙',
+        '🌝','🖤','💚'
+    ],
+
     PREFIX: '.',
     MAX_RETRIES: 3,
+
     GROUP_INVITE_LINK: 'https://chat.whatsapp.com/LrRAeEBvE8e8Arx34EEAY0?mode=wwc',
     ADMIN_LIST_PATH: './admin.json',
+
     IMAGE_PATH: 'https://files.catbox.moe/9osizy.jpg',
+
     NEWSLETTER_JID: '120363405860789177@g.us',
     NEWSLETTER_MESSAGE_ID: '428',
+
     OTP_EXPIRY: 300000,
     NEWS_JSON_URL: '',
+
     BOT_NAME: '𝐙𝙰𝙽𝚃𝙰 𝚇 𝐌𝙳',
     OWNER_NAME: '𝐒𝚄𝚁𝙰𝙽𝙶𝙰 𝐂𝙷𝙰𝙼𝙸𝚃𝙷',
-    OWNER_NUMBER: '94753708234',
+
     BOT_VERSION: '1.0.0',
     BOT_FOOTER: '> 𝐏𝙾𝚆𝙴𝚁𝙴𝙳 𝐁𝚈 𝐙𝙰𝙽𝚃𝙰 𝚇 𝐌𝙳 𝐌𝙸𝙽𝙸 𝐁𝙾𝚃*',
+
     CHANNEL_LINK: 'https://whatsapp.com/channel/0029Vb74RsT0wajtU4dOJD11',
 };
